a4cd4087212dfe0f606794615e310f0e00e3b860
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-11-23 21:30:35 +0530

    🎢 update bg-image on contact us

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -5,7 +5,7 @@ import { Footer } from './Footer';
 
 export const ContactUs: FC = () => {
   return (
-    <div className='bg-[url("/images/img_bg_4.jpg")] bg-no-repeat relative bg-cover bg-center bg-fixed transition-all w-full md:w-10/12 h-[400px] mx-auto my-20 rounded-lg'>
+    <div className='bg-[url("/images/bg-banner.JPG")] bg-no-repeat relative bg-cover bg-left bg-fixed transition-all w-full md:w-10/12 h-[400px] mx-auto my-20 rounded-lg'>
       <div className="absolute top-0 right-0 left-0 bottom-0 bg-black opacity-50"></div>
 
       <article className="w-full h-full flex justify-center items-center px-2">
