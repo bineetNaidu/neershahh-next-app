fb6e23778f344beeb5ec0fe7cf2709c3c62d9c9f
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-11-23 20:03:04 +0530

    ☘️ add website link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
 import { FaInstagram, FaFacebook, FaYoutube } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
+import { FaGlobe } from 'react-icons/fa';
 import { Button } from './ui/button';
 
 export const Footer: FC = () => {
@@ -20,6 +21,11 @@ export const Footer: FC = () => {
         <Button variant={'outline'} className="rounded-full p-3">
           <FaInstagram />
         </Button>
+        <a href="https://tapover.taplink.ws/" target="_blank">
+          <Button variant={'outline'} className="rounded-full p-3">
+            <FaGlobe />
+          </Button>
+        </a>
         <Button variant={'outline'} className="rounded-full p-3">
           <FaXTwitter />
         </Button>
